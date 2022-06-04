d12f25f9b13af40fde6f3f972a8ca7bcc643c905
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-04 12:18:22 +1000

    setup eslint & prettier

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { render } from "react-dom";
-import { createInertiaApp } from "@inertiajs/inertia-react";
-import "./index.css";
-const pages = import.meta.glob("../Pages/**/*.tsx");
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import './index.css'
+const pages = import.meta.glob('../Pages/**/*.tsx')
 
 async function resolvePage(name: string) {
-  const page = pages[`../Pages/${name}.tsx`];
+  const page = pages[`../Pages/${name}.tsx`]
   if (!page) {
     throw new Error(
       `Unknown page ${name}. Is is located under Pages with tsx extension ?`
-    );
+    )
   }
-  const content = await page();
-  return content.default;
+  const content = await page()
+  return content.default
 }
 createInertiaApp({
   resolve: resolvePage,
   setup({ el, App, props }) {
-    render(<App {...props} />, el);
+    render(<App {...props} />, el)
   },
-});
+})
