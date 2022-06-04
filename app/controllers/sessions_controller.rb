a7435cee9431964310eb3edfac9cbe1523d60fd7
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-05 07:45:30 +1000

    sign in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,17 @@ class SessionsController < ApplicationController
     render inertia: 'Login'
   end
 
-  def create; end
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user&.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path, inertia: { errors: 'Invalid credentials' }
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
 end
--- app/frontend/Pages/Login.tsx
+++ app/frontend/Pages/Login.tsx
@@ -2,10 +2,6 @@ import * as React from 'react'
 import { Inertia } from '@inertiajs/inertia'
 import { usePage } from '@inertiajs/inertia-react'
 
-interface ErrorResponse {
-  email?: string
-  password?: string
-}
 
 interface HasNameAndValue {
   name: string
@@ -13,7 +9,7 @@ interface HasNameAndValue {
 }
 
 function Login() {
-  const { errors }: { errors: ErrorResponse } = usePage().props
+  const { errors }: { errors: string } = usePage().props
   const [values, setValues] = React.useState({})
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -26,7 +22,7 @@ function Login() {
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
-    Inertia.post('/users', { user: values })
+    Inertia.post('/sessions', { user: values })
   }
   return (
     <div className="flex h-full flex-col items-center justify-center gap-10">
@@ -42,7 +38,6 @@ function Login() {
           className="px-4 py-2"
           onChange={handleChange}
         />
-        {errors?.email && <div className="text-red-500">{errors.email}</div>}
         <input
           type="password"
           name="password"
@@ -50,9 +45,7 @@ function Login() {
           className="px-4 py-2"
           onChange={handleChange}
         />
-        {errors?.password && (
-          <div className="text-red-500">{errors.password}</div>
-        )}
+        {errors && <div className="text-red-500">{errors}</div>}
         <button
           type="submit"
           className="mt-10 bg-primary px-3 py-2 text-xl text-white"
