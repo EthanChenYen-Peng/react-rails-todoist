69ef251a6e6264a784665b23db034219b8c7ddcc
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-05 08:27:17 +1000

    refactor register form to use inertia form helper

--- app/frontend/Pages/Register.tsx
+++ app/frontend/Pages/Register.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Inertia } from '@inertiajs/inertia'
-import { usePage } from '@inertiajs/inertia-react'
+import { usePage, useForm } from '@inertiajs/inertia-react'
 
 interface ErrorResponse {
   username?: string
@@ -14,24 +14,24 @@ interface HasNameAndValue {
 }
 
 function Register() {
-  const { errors }: { errors: ErrorResponse } = usePage().props
-  const [values, setValues] = React.useState({})
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    const target = e.target as HasNameAndValue
-    setValues({
-      ...values,
-      [target.name]: target.value,
-    })
-  }
-
+  const { transform, setData, post, processing, errors } = useForm({
+    email: '',
+    password: '',
+    username: '',
+  })
+  transform((data) => {
+    return {
+      user: data,
+    }
+  })
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
-    Inertia.post('/users', { user: values })
+    post('/users')
   }
   return (
     <div className="flex h-full flex-col items-center justify-center gap-10">
       <h2 className="text-3xl">Register</h2>
+      {processing && 'Processing'}
       <form
         className="flex w-[90%] max-w-[500px] flex-col gap-4 rounded-lg bg-gray-300 p-10"
         onSubmit={handleSubmit}
@@ -41,7 +41,7 @@ function Register() {
           name="username"
           placeholder="username"
           className="px-4 py-2"
-          onChange={handleChange}
+          onChange={(e) => setData('username', e.target.value)}
         />
         {errors?.username && (
           <div className="text-red-500">{errors.username}</div>
@@ -51,7 +51,7 @@ function Register() {
           name="email"
           placeholder="email"
           className="px-4 py-2"
-          onChange={handleChange}
+          onChange={(e) => setData('email', e.target.value)}
         />
         {errors?.email && <div className="text-red-500">{errors.email}</div>}
         <input
@@ -59,7 +59,7 @@ function Register() {
           name="password"
           placeholder="password"
           className="px-4 py-2"
-          onChange={handleChange}
+          onChange={(e) => setData('password', e.target.value)}
         />
         {errors?.password && (
           <div className="text-red-500">{errors.password}</div>
