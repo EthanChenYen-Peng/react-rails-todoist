e5a7fd41ebcdb682c01f20487c61ee5c9f8d043e
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-05 11:21:52 +1000

    adding basic style to task item

--- app/frontend/components/Task/TaskList.tsx
+++ app/frontend/components/Task/TaskList.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { MdDone} from 'react-icons/md'
 import type { Task } from './typse'
 
 interface Props {
@@ -7,7 +8,12 @@ interface Props {
 function TaskList({ tasks }: Props) {
   return (
     <div>
-      <p>TaskList</p>
+      {tasks.map((task)=> (
+        <div className='border-b-[1px] border-gray-400 py-5 flex items-center'>
+          <MdDone className='w-7 h-7 p-1 rounded-full translate-y-[1px] bg-inherit border-[1px] border-gray-500 text-gray-200 hover:text-gray-500 transition-colors duration-300 mr-2 cursor-pointer' />
+          {task.name}
+        </div>
+      ))}
     </div>
   )
 }
