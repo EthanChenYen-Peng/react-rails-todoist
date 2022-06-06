c409563fc40b89922fca151961baefde0886b752
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-06 19:39:37 +1000

    adding modal header icon

--- app/frontend/components/Modal/TaskEditModal.tsx
+++ app/frontend/components/Modal/TaskEditModal.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Dialog } from '@headlessui/react'
 import Modal from './Modal'
 import type { Task } from '../Task/types'
+import {
+  AiOutlineClose,
+  AiOutlineArrowUp,
+  AiOutlineArrowDown,
+} from 'react-icons/ai'
 
 interface Props {
   open: boolean
@@ -11,7 +16,17 @@ interface Props {
 export default function TaskEditModal({ open, onClose, task }: Props) {
   return (
     <Modal open={open} onClose={onClose}>
-      <Dialog.Panel className="mx-auto w-6/12 rounded bg-white px-4 py-2">
+      <Dialog.Panel className="mx-auto w-6/12 rounded-lg bg-white py-2">
+        <div className="flex border-b-[1px] border-b-gray-200 px-4 pt-3 pb-4">
+          <div className="ml-auto flex gap-3 text-3xl text-gray-600">
+            <AiOutlineArrowDown className="rounded-md p-[2px] transition-colors hover:bg-red-100" />
+            <AiOutlineArrowUp className="rounded-md p-[2px] transition-colors hover:bg-red-100" />
+            <AiOutlineClose
+              className="rounded-md p-[2px] transition-colors hover:bg-red-100"
+              onClick={onClose}
+            />
+          </div>
+        </div>
         {task.name}
       </Dialog.Panel>
     </Modal>
