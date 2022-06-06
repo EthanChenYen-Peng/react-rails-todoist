27e194fdfb50ea7cc75da32f0d16d8d4ac9c835a
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-06 11:34:48 +1000

    adding annotate gem

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id           :bigint           not null, primary key
+#  user_id      :bigint
+#  name         :string           not null
+#  description  :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  completed_at :datetime
+#  due_date     :datetime
+#
 class Task < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id           :bigint           not null, primary key
+#  user_id      :bigint
+#  name         :string           not null
+#  description  :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  completed_at :datetime
+#  due_date     :datetime
+#
 FactoryBot.define do
   factory :task do
     name { Faker::Lorem.sentence }
