1673780bb0c018bd98851679b354fb75d2ab7136
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-06 13:14:31 +1000

    setup react date picker

--- app/frontend/components/Task/Datepicker.tsx
+++ app/frontend/components/Task/Datepicker.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 interface Props {
   open: boolean
 }
 function Datepicker({ open }: Props) {
-  return <>{open && <p>Date picker</p>}</>
+  const [startDate, setStartDate] = React.useState(new Date())
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date: Date) => setStartDate(date)}
+    />
+  )
 }
 
 export default Datepicker
