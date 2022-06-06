680dd4b6d36736bf7fbf506da9d7291709713bde
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-06 18:28:56 +1000

    adding due date text color

--- app/frontend/components/Task/TaskDueDate.tsx
+++ app/frontend/components/Task/TaskDueDate.tsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import type { Task } from './types'
 import moment from 'moment'
 import { BsCalendarDate } from 'react-icons/bs'
 
-function formatDateString(dateString: string | null) {
-  console.log(dateString)
-  if (!dateString) return
-  const dateObj = new Date(dateString)
-  return moment(dateObj).format('MMM DD')
-}
-
-function TaskDueDate({ task }: Task) {
+function TaskDueDate({ task }: { task: Task }) {
+  let formattedDateString
+  let color
+  if (task.due_date) {
+    const dateObj = new Date(task.due_date)
+    formattedDateString = moment(dateObj).format('MMM DD')
+    if (moment(dateObj).isBefore(Date.now(), 'day')) {
+      color = 'text-red-500'
+    } else if (moment(dateObj).isAfter(Date.now(), 'day')) {
+      color = 'text-purple-500'
+    } else {
+      color = 'text-black'
+    }
+  }
   return (
     <>
       {task.due_date && (
-        <div className="flex items-center gap-2 text-sm text-purple-500">
+        <div className={`${color} flex items-center gap-2 text-sm`}>
           <BsCalendarDate />
-          {formatDateString(task.due_date)}
+          {formattedDateString}
         </div>
       )}
     </>
